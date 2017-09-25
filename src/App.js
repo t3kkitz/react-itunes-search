b66d1d184016e9aa524fea2d648be64222f31671
@t3kkitz Sergey Bulgakov <t3kkitz@gmail.com>
2017-09-25 16:08:38 +0300

    remake

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
 
 class App extends Component {
+  state = {
+    media: 'all',
+    query: ''
+  }
+
   render() {
+
     return (
       <div className="App">
-        <Header />
-        <Main />
+        <form>
+          <input type="text"/>
+          <select value={this.state.media} onChange={this.handleMediaChange}>
+            <option value="movie">Movie</option>
+            <option value="podcast">Podcast</option>
+            <option value="music">Music</option>
+            <option value="musicVideo">MusicVideo</option>
+            <option value="audiobook">Audiobook</option>
+            <option value="shortFilm">ShortFilm</option>
+            <option value="tvShow">TV Show</option>
+            <option value="software">Software</option>
+            <option value="ebook">E-book</option>
+            <option value="all">All</option>
+          </select>
+        </form>
       </div>
     );
   }
+
+  handleMediaChange = (e) => {
+    this.setState({media: e.target.value})
+  }
+
 }
 
 export default App;
+
+//https://itunes.apple.com/search?media=all&term=maroon+5
