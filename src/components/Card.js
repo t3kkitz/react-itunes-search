dde90a6ea6bfcb8d4eb320dc08533b6603918df2
@t3kkitz Sergey Bulgakov <t3kkitz@gmail.com>
2017-09-25 14:22:17 +0300

    add elements

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+background:#fff;
+`;
+
+function Card() {
+  return (
+    <Wrapper />
+  );
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import CardList from './CardList'
+
+const Wrapper = styled.main`
+  background:#eee;
+  min-height: 600px;
+`;
 
 function Main() {
   return (
-    <div>
-      <h2>main</h2>
-    </div>
+    <Wrapper>
+      <CardList/>
+    </Wrapper>
   );
 }
 
